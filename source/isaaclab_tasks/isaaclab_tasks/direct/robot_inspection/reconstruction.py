89e5dc3f70482603c0efafd276dad208d477762c
Noa Chapman <nchapmancsr25@gmail.com>
2026-01-30 12:32:12 -0700

    Initial Commit

---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/robot_inspection/reconstruction.py
@@ -0,0 +1,11 @@
+# Perform reconstruction of the rubiks cube from an rgb pointcloud gathered from the inspection cameras
+
+import numpy as np
+import open3d as o3d
+
+class RubiksReconstruction:
+    def __init__(self):
+        pass
+
+    def reconstruct(self, pointcloud):
+        pass
